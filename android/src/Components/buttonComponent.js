0fea156b7f3d7c6447cb784ded68e9d77e986256
bessonglawrence <orockbessonglawrence@gmail.com>
2022-09-19 22:17:27 +0100

    created the buttonComponent and the main Home screen

---
+++ android/src/Components/buttonComponent.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const buttonComponent = () => {
+  return (
+    <View>
+      <Text>buttonComponent</Text>
+    </View>
+  )
+}
+
+export default buttonComponent
+
+const styles = StyleSheet.create({})
